2f0d0fb6e72b9e51fe1f8749c7cda80f3f1c831f
mkyurw <michaelyu103@gmail.com>
2020-07-05 16:17:01 +0800

    push original file 2020/7/5

---
+++ checkmypwd.py
@@ -0,0 +1,43 @@
+import requests
+import hashlib
+import sys
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/'+ query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+def get_password_leak_count(hashes, hash_to_check):
+    # use text() to read the response, which is the hashes
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leak_count(response, tail)
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times. you should change your password')
+        else:
+            print(f'{password} was not found. Carry on!')
+
+
+if __name__ == '__main__':
+    with open('pwd.txt','r') as pwd:
+        next(pwd)
+        pwd_list = []
+        for lines in pwd:
+            lines = lines.strip('\n')[1:-1]
+            pwd_list.append(lines)
+            main(lines.splitlines())
+        print('Check is done!')
